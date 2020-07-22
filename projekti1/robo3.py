bac1f4c2ae9ae43d992a0a94cf09e0763aa7a582
MitjaHaimila <mitja.haimila@gmail.com>
2020-07-22 15:59:04 +0300

    aaa

--- projekti1/robo3.py
+++ projekti1/robo3.py
@@ -1,7 +1,7 @@
 # #!/usr/bin/env python3
 from ev3dev2.motor import LargeMotor, OUTPUT_B, OUTPUT_C, SpeedPercent, MoveTank
 # from ev3dev2.sensor import INPUT_1
-from ev3dev2.sensor.lego import TouchSensor
+# from ev3dev2.sensor.lego import TouchSensor
 from ev3dev2.led import Leds
 from ev3dev2.sound import Sound
 from threading import Thread
